862794d01c48954467da7098f81c27b4b3198882
David Tejuosho <tejuoshodavid@gmail.com>
2026-01-15 16:42:58 -0800

    orbstack install

--- zprofile.zsh
+++ zprofile.zsh
@@ -3,3 +3,7 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 if [ -f ~/scripts/aliases.zsh ]; then
     . ~/scripts/aliases.zsh
 fi
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init.zsh 2>/dev/null || :
